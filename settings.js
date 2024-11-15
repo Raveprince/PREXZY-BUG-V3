51f718fea977aaf1f224086a4b442eb0d322c5bd
Raveprince <semilore631@gmail.com>
2024-11-15 18:32:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2347063956321'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2349069259027'  //creator number
+global.ownername = 'HEISRAVEPRINCE🦇🌹💦' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
